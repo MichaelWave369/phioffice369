7ea49b01a85fa586a2b1c774a29c015cd15f7e2a
Michael Hughes <microneesia79@gmail.com>
2026-05-31 16:10:09 -0700

    Wrap app with first-run onboarding gate

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import AppErrorBoundary from './components/AppErrorBoundary.jsx';
+import OnboardingGate from './components/OnboardingGate.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <App />
+      <OnboardingGate>
+        <App />
+      </OnboardingGate>
     </AppErrorBoundary>
   </React.StrictMode>,
 );
